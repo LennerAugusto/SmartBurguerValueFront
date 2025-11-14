6b01456c7d68e039f646cda9c28c57f6c5b80213
LennerAugusto <Lenner_augusto22@hotmail.com.br>
2025-11-13 22:54:11 -0300

    Fix:layout-input-file-purchase

--- SmartBurguerValueFront/obj/Debug/net9.0/SmartBurguerValueFront.AssemblyInfo.cs
+++ SmartBurguerValueFront/obj/Debug/net9.0/SmartBurguerValueFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartBurguerValueFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff09455743fa41e99592ede2d358b4d890f803a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e67ed52e3709ddeb5f909d01f7305fd091e13984")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartBurguerValueFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartBurguerValueFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
